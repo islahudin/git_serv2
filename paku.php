33eb238a72538f49c98971619bee86d7c64b63c4
islahudin <islahudin.soft01engineer@gmail.com>
2025-01-26 19:04:37 +0700

    update 16

--- paku.php
+++ paku.php
@@ -7,22 +7,22 @@ if (function_exists('shell_exec')) {
     echo "shell_exec is disabled";
 }
 
-// $output = shell_exec('whoami'); // Periksa user yang digunakan
-// // file_put_contents('/path/to/logs/user_check.log', $output);
-// file_put_contents('/home/n1577716/public_html/logs/user_check.log', $output);
+$output = shell_exec('whoami'); // Periksa user yang digunakan
+// file_put_contents('/path/to/logs/user_check.log', $output);
+file_put_contents('/home/n1577716/public_html/logs/user_check.log', $output);
 
-$output = "This is a test log content.";
-$logPath = '/home/n1577716/public_html/logs/user_check.log';
+// $output = "This is a test log content.";
+// $logPath = '/home/n1577716/public_html/logs/user_check.log';
 
-// Tulis ke file
-$result = file_put_contents($logPath, $output);
+// // Tulis ke file
+// $result = file_put_contents($logPath, $output);
 
-// Cek apakah berhasil
-if ($result === false) {
-    echo "Gagal menulis log ke file: $logPath";
-} else {
-    echo "Berhasil menulis log ke file: $logPath (" . $result . " bytes)";
-}
+// // Cek apakah berhasil
+// if ($result === false) {
+//     echo "Gagal menulis log ke file: $logPath";
+// } else {
+//     echo "Berhasil menulis log ke file: $logPath (" . $result . " bytes)";
+// }
 
 ?>
 <!-- cd /public_html/gitremote.qordinate.com/git_serv2 && git pull origin main -->
