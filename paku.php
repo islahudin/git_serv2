dd61d364ce86c4a47cfb7f5df59e37ad0b756ae7
islahudin <islahudin.soft01engineer@gmail.com>
2025-01-26 18:47:48 +0700

    update 12

--- paku.php
+++ paku.php
@@ -7,5 +7,8 @@ if (function_exists('shell_exec')) {
     echo "shell_exec is disabled";
 }
 
+$output = shell_exec('whoami'); // Periksa user yang digunakan
+file_put_contents('/path/to/logs/user_check.log', $output);
 
 ?>
+<!-- cd /public_html/gitremote.qordinate.com/git_serv2 && git pull origin main -->
